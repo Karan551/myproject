4dd6f3c669e93f06ab910ecd52f7d2ac85e6d3b3
Kartik <iamkaranmishra551@gmail.com>
2024-02-20 00:41:23 +0530

    adding url and slug .

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+# This will use to identify if multiple apps are availabel.
+app_name="post"
 urlpatterns = [
     path("", views.post_list, name="post_list"),
+    path("<slug:slug>", views.post_page, name="page"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import HttpResponse
 
 
 # Create your views here.
 def post_list(request):
-    return render(request, "posts/post_list.html")
+    posts = Post.objects.all().order_by("-date")
+    context = {"posts": posts}
+    return render(request, "posts/post_list.html", context)
+
+
+def post_page(request, slug):
+    post = Post.objects.get(slug=slug)
+    print("This is my first page:",post)
+    return render(request, "posts/post_page.html",{"post":post})
+
+    # return HttpResponse(slug)
