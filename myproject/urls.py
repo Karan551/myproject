a8298abd9f1e64eacdaf7c49a13c64702ec02383
Kartik <iamkaranmishra551@gmail.com>
2024-03-13 12:07:08 +0530

    Adding users app.

--- myproject/urls.py
+++ myproject/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
     path("posts/", include("posts.urls")),
+    path("users/", include("users.urls")),
 ]
 # To use media files (serving files during development)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
