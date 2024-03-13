3ec3e1f95f33cf21cee7ef251cb69461bdf83f5d
Kartik <iamkaranmishra551@gmail.com>
2024-03-13 21:42:50 +0530

    Adding register route in users app.

--- users/views.py
+++ users/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import RegisterForm
+
+# from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 
 def register(request):
-    return render(request, "users/register.html")
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Form is submitted successfully.")
+    else:
+        form = RegisterForm()
+    # print("This is form", form)
+    return render(request, "users/register.html", {"form": form})
