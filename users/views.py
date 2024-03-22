f2ae34571ec292709fa13a70d0868ad555a5af58
Kartik <iamkaranmishra551@gmail.com>
2024-03-22 23:51:41 +0530

    adding login and logout function.

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
+from django.contrib.auth import logout, login
 
 # from django.contrib.auth.forms import UserCreationForm
 
@@ -12,7 +13,25 @@ def register(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponse("Form is submitted successfully.")
+            return redirect("/users/login/")
     else:
         form = RegisterForm()
     return render(request, "users/register.html", {"form": form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            print("This is form.get_user()", form.get_user())
+            login(request, form.get_user())
+            return redirect("post:post_list")
+            # return HttpResponse("Form is submitted.")
+    else:
+        form = LoginForm()
+    return render(request, "users/login.html", {"form": form})
+
+
+def logout_user(request):
+    logout(request)
+    return redirect("/")
